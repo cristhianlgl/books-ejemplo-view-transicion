43166139a59510c14f7732c8e0512120eb3c45bc
cristhianlgl <cristhianlgl@hotmail.com>
2023-07-06 09:32:20 -0500

    agregar estilos y contenido de la pagina

---
+++ src/data/books.js
@@ -0,0 +1,25 @@
+export const books = [
+	{
+		title: 'Clean Code',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		slug: 'clean-code',
+		author: 'Robert C. Martin',
+		// eslint-disable-next-line quotes
+		description: "Even bad code can function. But if code isn't clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn't have to be that way"
+	},
+	{
+		title: 'The Pragmatic Programmer: From Journeyman to Master',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+		slug: 'the-pragmatic-programmer'
+	},
+	{
+		title: 'JavaScript: The Good Parts',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+		slug: 'javascript-the-good-parts'
+	},
+	{
+		title: 'Clean Architecture',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1471680093i/18043011.jpg',
+		slug: 'clean-architecture'
+	}
+]
